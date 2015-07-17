7a604e6b577a136962213317f635ce1fee4d8f51
Perry <chenzhipeng901105@gmail.com>
2015-07-17 14:18:53 +0800

    add initial code

---
+++ MoonRunner/MoonRunner/NewRunViewController.swift
@@ -0,0 +1,87 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import CoreData
+
+let DetailSegueName = "RunDetails"
+
+class NewRunViewController: UIViewController {
+  var managedObjectContext: NSManagedObjectContext?
+
+  var run: Run!
+
+  @IBOutlet weak var promptLabel: UILabel!
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var distanceLabel: UILabel!
+  @IBOutlet weak var paceLabel: UILabel!
+  @IBOutlet weak var startButton: UIButton!
+  @IBOutlet weak var stopButton: UIButton!
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+
+    startButton.hidden = false
+    promptLabel.hidden = false
+
+    timeLabel.hidden = true
+    distanceLabel.hidden = true
+    paceLabel.hidden = true
+    stopButton.hidden = true
+  }
+
+  @IBAction func startPressed(sender: AnyObject) {
+    startButton.hidden = true
+    promptLabel.hidden = true
+
+    timeLabel.hidden = false
+    distanceLabel.hidden = false
+    paceLabel.hidden = false
+    stopButton.hidden = false
+  }
+
+  @IBAction func stopPressed(sender: AnyObject) {
+    let actionSheet = UIActionSheet(title: "Run Stopped", delegate: self, cancelButtonTitle: "Cancel", destructiveButtonTitle: nil, otherButtonTitles: "Save", "Discard")
+    actionSheet.actionSheetStyle = .Default
+    actionSheet.showInView(view)
+  }
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if let detailViewController = segue.destinationViewController as? DetailViewController {
+      detailViewController.run = run
+    }
+  }
+}
+
+// MARK: UIActionSheetDelegate
+extension NewRunViewController: UIActionSheetDelegate {
+  func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+    //save
+    if buttonIndex == 1 {
+      performSegueWithIdentifier(DetailSegueName, sender: nil)
+    }
+      //discard
+    else if buttonIndex == 2 {
+      navigationController?.popToRootViewControllerAnimated(true)
+    }
+  }
+}
